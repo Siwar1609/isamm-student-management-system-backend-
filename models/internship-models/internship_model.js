ad391b0435a573fb3c10fc9f7834957b993934e7
oumaimamzoughi <oumaymaamzoughi@gmail.com>
2024-12-01 23:00:04 +0200

    ajout de schema internship

--- models/internship-models/internship_model.js
+++ models/internship-models/internship_model.js
@@ -1,24 +1,47 @@
-// import mongoose from "mongoose";
 import mongoose from 'mongoose'
 
-const internshipSchema = mongoose.Schema({
-  startDate: {
-    type: Date,
+const InternshipSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  périodeOuverte: { type: Boolean, required: true },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
+  academicYear: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'AcademicYear',
     required: true,
   },
-  endDate: {
-    type: Date,
+  level: {
+    type: String,
+    enum: ['1st year', '2nd year'], // Academic levels
     required: true,
   },
-  status: {
-    type: String,
-    enum: ['ended', 'pending', 'active'],
+  teacherId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Teacher',
+    required: true,
   },
-  name: {
-    type: String,
-    enum: ['PFA', 'PFE', 'Stage', 'option'], // Liste des options possibles
+  studentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Student',
+    required: true,
+  },
+  published: { type: Boolean, required: true },
+  Validate: {
+    value: { type: Boolean, required: true },
+    reason: { type: String },
+  },
+  documents: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Document',
+      required: false,
+    },
+  ],
+  periodeId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Period',
     required: true,
   },
 })
 
-export default mongoose.model('Internship', internshipSchema)
+export default mongoose.model('Internship', InternshipSchema)
