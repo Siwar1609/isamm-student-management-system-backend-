14fe2988cb93ff7fe92e32a1e14184d7ecbf0799
oumaimamzoughi <oumaymaamzoughi@gmail.com>
2024-12-10 16:22:32 +0200

    add routes of US4

--- routes/internship-routes/internship_route.js
+++ routes/internship-routes/internship_route.js
@@ -16,7 +16,10 @@ router.put('/:id/open', accessByRole(['admin']), updateInternship)
 router.get('/:id', accessByRole(['admin']), getInternshipById)
 router.get('/', accessByRole(['admin']), getAllInternships)
 router.delete('/:id', accessByRole(['admin']), deleteInternship)
-
+router.post('/:type/planning/assign ',accessByRole(['admin']), assignTeachersToInternship)
+router.patch('/:type/planning/update ', accessByRole(['admin']), updateInternshipPlanning)
+router.patch('/:type/planning/publish/:response ', accessByRole(['admin']), publishOrMaskPlanning)
+router.patch('/:type/planning/send ', accessByRole(['admin']), sendInternshipPlanningEmail)
 router.get('/students/all', accessByRole(['admin']), getAllStudents)
 
 export default router
