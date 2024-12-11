e02b6baf7ebca7a9fc8409657857c986757a0ce8
Ahmed <ahmedgafsi2.0@gmail.com>
2024-12-11 22:04:01 +0100

    fixing some missing stuff

--- services/students_services.js
+++ services/students_services.js
@@ -13,3 +13,26 @@ export const addStudent = async function (value) {
   // saving the user
   return await newStudent.save()
 }
+
+export const getStudents = async function () {
+  const students = await Student.find()
+  return students
+}
+
+export const getStudent = async function (id) {
+  const student = await Student.findById(id)
+  return student
+}
+
+export const updateStudent = async function (id, student) {
+  const updatedStudent = await Student.findByIdAndUpdate(id, student, {
+    new: true,
+  })
+
+  return updatedStudent
+}
+
+export const deleteStudent = async function (id) {
+  await Student.findByIdAndDelete(id)
+  return { message: 'Student deleted successfully' }
+}
