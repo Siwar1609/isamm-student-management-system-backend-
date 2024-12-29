b9ed37653e919565eb5cda41d554b7697f479760
Oumaima Mzoughi <oumaymaamzoughi@gmail.com>
2024-12-28 14:04:10 -1100

    add route for US4.3 in option routes

--- routes/options-routes/option_routes.js
+++ routes/options-routes/option_routes.js
@@ -3,6 +3,7 @@ import {
   addOption,
   getAllOptions,
   getOptionsByStudentId,
+  publishOrMaskOption,
 } from '../../controllers/option-controller/option_controller.js'
 import { accessByRole } from '../../middlewares/users-middlewares/auth_middleware.js'
 
@@ -11,5 +12,6 @@ const router = express.Router()
 router.post('/', accessByRole(['student']), addOption)
 router.get('/', accessByRole(['admin']), getAllOptions)
 router.get('/:studentId', accessByRole(['admin']), getOptionsByStudentId)
+router.post('/publish/:response', accessByRole(['admin']), publishOrMaskOption)
 
 export default router
