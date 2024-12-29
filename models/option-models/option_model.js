587d16121803b1a81af798f467430a7d6c86c670
Oumaima Mzoughi <oumaymaamzoughi@gmail.com>
2024-12-28 13:10:59 -1100

    add field published to option schema

--- models/option-models/option_model.js
+++ models/option-models/option_model.js
@@ -59,6 +59,9 @@ const optionSchema = mongoose.Schema({
   webDevGrade: { type: Number, required: true },
   oopGrade: { type: Number, required: true },
   algorithmsGrade: { type: Number, required: true },
+  published: { 
+    type: Boolean, 
+    required: true },
 })
 
 export default mongoose.model('Option', optionSchema)
