a9ba8f1742c61819c9a2509900a6596885db35e1
Oumaima Mzoughi <oumaymaamzoughi@gmail.com>
2025-01-01 17:14:22 +0100

    add route US 5.1 to option_routes

--- routes/options-routes/option_routes.js
+++ routes/options-routes/option_routes.js
@@ -8,8 +8,9 @@ import {
   getClassementByOption,
   updateOptionResults,
   SentEmailFinalOption,
+  getFinalList,
 } from '../../controllers/option-controller/option_controller.js'
-import { accessByRole } from '../../middlewares/users-middlewares/auth_middleware.js'
+import { loggedMiddleware,accessByRole } from '../../middlewares/users-middlewares/auth_middleware.js'
 
 const router = express.Router()
 
@@ -20,6 +21,7 @@ router.post('/publish/:response', accessByRole(['admin']), publishOrMaskOption)
 router.post('/result', accessByRole(['admin']), calculateOptionResults)
 router.get('/order/:optionName', accessByRole(['admin']), getClassementByOption)
 router.patch('/:optionResultId', accessByRole(['admin']), updateOptionResults)
-router.get('/send/email', accessByRole(['admin']), SentEmailFinalOption)
+router.post('/send/email', accessByRole(['admin']), SentEmailFinalOption)
+router.get('/listFinalChoice/student', loggedMiddleware,accessByRole(['student']), getFinalList)
 
 export default router
