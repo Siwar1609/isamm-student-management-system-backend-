bf639ef5d67fad25c1bc1de3693d3503fc24e6f8
Ahmed Gafsi <ahmedgafsi2.0@gmail.com>
2025-05-17 00:44:41 +0100

    fixing chapter model / chapter controller

--- models/subject-models/chapter_model.js
+++ models/subject-models/chapter_model.js
@@ -1,29 +1,31 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 const ChapterSchema = new mongoose.Schema({
   order: { type: Number, required: true },
   title: { type: String, required: true },
+  status: {
+    type: String,
+    enum: ['not yet', 'in progress', 'completed'],
+    default: 'not yet'
+  },
+  statusUpdatedAt: { type: Date },
   section: [
     {
       content: { type: String },
       advancement: {
         type: String,
         enum: ['not yet', 'in progress', 'completed'],
-        default: 'not yet',
+        default: 'not yet'
       },
-      modificationDate: { type: Date }, // New field for section modification date
-    },
+      modificationDate: { type: Date },
+      statusUpdatedAt: { type: Date }
+    }
   ],
   subjectId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Subject',
-    required: true // Maintenant obligatoire
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-  completedDate: { type: Date }, // New field for chapter completion date
-})
+    required: true
+  }
+}, { timestamps: true });
 
-export default mongoose.model('Chapter', ChapterSchema)
+export default mongoose.model('Chapter', ChapterSchema);
