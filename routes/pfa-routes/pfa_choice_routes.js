b5c5548ba7e9f2cb0b5de9a0b0f14837e664a061
ilyes hafsi <hafsitsw@gmail.com>
2025-05-17 03:32:50 +0100

    hafsi

--- routes/pfa-routes/pfa_choice_routes.js
+++ routes/pfa-routes/pfa_choice_routes.js
@@ -15,19 +15,17 @@ import {
   sendEmailToRecipients,
   togglePublishPFA,
   list_pfa_by_teacher,
-  sorted_pfa  // Added the sorted_pfa import
-
-
-
+  sorted_pfa,
+  manualAssignPFA2  // Added the manualAssignPFA2 import
 } from '../../controllers/pfa-controller/choice_pfa_controller.js'
 
 const choice_pfa_route = express.Router()
 
-choice_pfa_route.post(
-  '/:pfaId/assign',
-  accessByRole(['admin']),
-  manualAssignPFA,
-)
+// choice_pfa_route.post(
+//   '/:pfaId/assign',
+//   accessByRole(['admin']),
+//   manualAssignPFA,
+// )
 // ----------------------- Student Routes --------------------------------------------------------
 choice_pfa_route.patch(
   '/:choiceId',
@@ -84,7 +82,10 @@ choice_pfa_route.get(
   fetchStudentChoices,
 )
 choice_pfa_route.post('/allocate', accessByRole(['admin']), autoAllocatePFA)
-choice_pfa_route.post('/assign', accessByRole(['admin']), manualAssignPFA)
+choice_pfa_route.post('/assign/:pfaId', accessByRole(['admin']), manualAssignPFA)
+// Add the new route for manualAssignPFA2
+choice_pfa_route.post('/assign2', accessByRole(['admin']), manualAssignPFA2)
+
 choice_pfa_route.patch(
   '/:id/publish',
   accessByRole(['admin']),
